a9b138f37077a531a9fac83eeef9448ef20e57d2
dangun <daniel.gunnarsson123@gmail.com>
2021-12-12 16:08:35 +0100

    Solutions for Day 12

---
+++ 12/12-2.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+paths = defaultdict(set)
+
+with open('input') as file:
+    for line in file:
+        line = line.strip().split('-')
+        paths[line[0]].add(line[1])
+        if line[0] != 'start':
+            paths[line[1]].add(line[0])
+
+def step(current, visited, visited_twice):
+    if current == 'end':
+        return 1
+    if current.islower():
+        visited = visited | {current}
+    result = 0
+    result += sum(step(node, visited, visited_twice) for node in paths[current] - visited)
+    if not visited_twice:
+        result += sum(step(node, visited, True) for node in paths[current] & visited)
+    return result
+
+paths = step('start', set(), False)
+
+print(f'Asnwer: {paths}')
